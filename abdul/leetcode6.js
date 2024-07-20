041188c3469750c91ad1c4ee19dbe19701347724
abdulsiddique <abdulsiddique486@gmail.com>
2024-07-20 16:07:30 +0530

    leetcode6

---
+++ abdul/leetcode6.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    outputArray = [];
+    for(let i=0;i<arr.length;i++){
+       if(fn(arr[i],i)){ 
+
+        outputArray.push(arr[i])
+     
+       }
+    
+    }
+    return outputArray;
+};
